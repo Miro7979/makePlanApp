b2d38b1d29068f19e2c1cf7cbe979eb505a3bf25
Mirwais Nezami <mirwais79@hotmail.com>
2020-02-12 14:47:51 +0100

    starting the project

---
+++ src/components/layout/navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import SignedInLinks from './signedInLinks';
+import SignedOutLinks from './signedOutLinks';
+
+const Navbar = () => {
+  return (
+    <nav className="nav-wrapper red darken-3">
+      <div className="container">
+        <Link to='/' className="brand-logo">Miros Plan Maker</Link>
+        <SignedInLinks />
+        <SignedOutLinks />
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar;
